d92332e81cb4759e8b70066b63419e1f5ac29f03
xd <asqhaqs@163.com>
2016-06-30 10:37:31 +0800

    jdbc 与 servlet

--- ServletProject/src/com/example/dao/impl/UserDaoImpl.java
+++ ServletProject/src/com/example/dao/impl/UserDaoImpl.java
@@ -2,6 +2,7 @@ package com.example.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.example.dao.UserDao;
@@ -11,7 +12,7 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void save(Connection conn, User user) throws SQLException {
-		// TODO Auto-generated method stub
+		
 		PreparedStatement ps = conn.
 				prepareStatement("INSERT INTO tbl_user(name,password,email) VALUES(?,?,?)");
 		ps.setString(1,user.getName());
@@ -22,7 +23,7 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void update(Connection conn, Long id, User user) throws SQLException {
-		// TODO Auto-generated method stub
+		
 		String updateSql = "UPDATE tbl_user SET name = ?,password = ?,email = ? WHERE id = ?";
 		PreparedStatement ps = conn.prepareStatement(updateSql);
 		ps.setString(1, user.getName());
@@ -34,11 +35,20 @@ public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void delete(Connection conn, User user) throws SQLException {
-		// TODO Auto-generated method stub
+		
 		PreparedStatement ps = conn.prepareStatement("DELETE FROM tbl_user WHERE id = ?");
 		ps.setLong(1, user.getId());
 		ps.execute();
 
+	}
+	@Override
+	public ResultSet get(Connection conn,User user)throws SQLException{
+		PreparedStatement ps = conn.prepareStatement("SELECT * FROM tbl_user WHERE name = ? AND password = ?");
+		ps.setString(1, user.getName());
+		ps.setString(2, user.getPassword());
+		
+		return ps.executeQuery();
+		
 	}
 
 }
